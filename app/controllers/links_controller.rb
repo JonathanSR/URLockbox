ea4044b234fb9c87c15f8cf91b392ce71d2e753f
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-05-22 16:14:09 -0600

    add link is working correctly, not crossed with edit anymore
    edit tests are still failing, now sad path create link test is also failing

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,9 +1,5 @@
 class LinksController < ApplicationController
-
-  def new
-  end
-
-
+  before_action :get_link, only: [:update, :edit]
   def index
     if !current_user
       redirect_to login_path
@@ -18,18 +14,15 @@ class LinksController < ApplicationController
     if @link.save
       redirect_to root_path
     else
-      flash.now[:error] = @link.errors.full_messages.join('. ')
-      render :index
+      flash[:error] = @link.errors.full_messages.join('. ')
+      redirect_to :back
     end
   end
 
   def edit
-    @link = current_user.links.find_by_id(params[:id])
   end
 
   def update
-    @link = current_user.links.find_by_id(params[:id])
-    
     if @link.update(link_params)
       flash[:success] = "Link has been updated!"
       redirect_to root_path
@@ -39,7 +32,12 @@ class LinksController < ApplicationController
     end
   end
 
+private
   def link_params
     params.require(:link).permit(:url, :title)
+  end
+
+  def get_link
+    @link = current_user.links.find_by_id(params[:id])
   end
 end
