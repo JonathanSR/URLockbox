4b01b19aad747f540f35d82aefc8c94bb3ae51f9
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-05-22 16:42:10 -0600

    fixes users migration,
    adds password confirmation and fixes rest of application affected by it

--- db/schema.rb
+++ db/schema.rb
@@ -28,8 +28,9 @@ ActiveRecord::Schema.define(version: 20170522155353) do
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "password_confirmation"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
   end
 
 end
