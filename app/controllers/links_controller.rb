436ff26c4e5636763f8ca8b3c811e6ef7f0897fb
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-05-22 13:17:38 -0600

    adds new link form to root page
    adds validate_url gem with setup procedures.
    adds css styling for displayed urls.
    adds create method to links controller.
    creates user and links relationship.
    modified routes accordingly.
    adds testing for creation of links and sad path test for invalid url.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,9 +1,29 @@
 class LinksController < ApplicationController
 
+  def new
+  end
+
+
   def index
     if !current_user
       redirect_to login_path
     else
+    @link = Link.new
     end
+  end
+
+  def create
+    @link = current_user.links.new(link_params)
+
+    if @link.save
+      redirect_to root_path
+    else
+      flash.now[:error] = @link.errors.full_messages.join('. ')
+      render :index
+    end
+  end
+
+  def link_params
+    params.require(:link).permit(:url, :title)
   end
 end
---
+++ spec/features/user/user_views_link_form_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe "logged in user" do
+  context "visits '/' page" do
+    it "creates a new link" do
+      user= User.create(email:"sample@google.com", password:"password")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit("/")
+
+      expect(page).to have_content("Url")
+      expect(page).to have_content("Title")
+      expect(page).to have_button("Add Link")
+
+      fill_in "link[url]", with: "https://www.google.com/"
+      fill_in "link[title]", with: "Google"
+      click_on("Add Link")
+
+      expect(current_path).to eq('/')
+      expect(page).to have_content("google.com")
+      expect(page).to have_content("Google")
+      expect(page).to have_content("false")
+    end
+  end
+
+  it "uses ivalid url to create link" do
+    user= User.create(email:"sample@google.com", password:"password")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit("/")
+
+    fill_in "link[url]", with: "google.com/"
+    fill_in "link[title]", with: "Google"
+    click_on("Add Link")
+
+    expect(current_path).to eq('/links')
+    expect(page).to have_content("Url is not a valid URL")
+  end
+end
